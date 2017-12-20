10c9691f848f449208f018c54f68c37c2b019308
Anastasia Mishin <am988@afsconnect1.njit.edu>
2017-12-20 00:43:35 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include 'view/header.php'; ?>
+
+<html>
+
+<body> 
+
+	<div class = "container">
+		<h1> Welcome to your To-Do List </h1>
+		<div class = "center_button">
+			<a href = "view/signin.php" class="button"> Sign In </a>
+			<a href = "view/register.php" class="button"> Register </a>
+		</div>
+	</div>
+
+</body>
+
+</html>
+
+<?php include 'view/footer.php'; ?>
